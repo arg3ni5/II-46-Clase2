5adc1e4ad4df258015c229f777338c8241498d9c
Fabian Chinchilla Mayorga <fabian.chinchillam@euromobilia.com>
2025-05-23 21:15:53 -0600

    Agregar archivos de proyecto.

---
+++ ClaseEjemplo/Models/Persona.vb
@@ -0,0 +1,8 @@
+﻿Public Class Persona
+    Private _nombre As String
+    Private _apellido As String
+    Private _edad As Integer
+    Private _direccion As String
+    Private _telefono As String
+    Private _email As String
+End Class
