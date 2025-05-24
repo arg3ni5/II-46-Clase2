afb2ed02cba9086469505e9ae4c09fbd1db0b1fa
Fabian Chinchilla Mayorga <fabian.chinchillam@euromobilia.com>
2025-05-23 21:20:03 -0600

    Encapsulando Persona

--- ClaseEjemplo/Models/Persona.vb
+++ ClaseEjemplo/Models/Persona.vb
@@ -5,4 +5,58 @@
     Private _direccion As String
     Private _telefono As String
     Private _email As String
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As String
+        Get
+            Return _apellido
+        End Get
+        Set(value As String)
+            _apellido = value
+        End Set
+    End Property
+
+    Public Property Edad As Integer
+        Get
+            Return _edad
+        End Get
+        Set(value As Integer)
+            _edad = value
+        End Set
+    End Property
+
+    Public Property Direccion As String
+        Get
+            Return _direccion
+        End Get
+        Set(value As String)
+            _direccion = value
+        End Set
+    End Property
+
+    Public Property Telefono As String
+        Get
+            Return _telefono
+        End Get
+        Set(value As String)
+            _telefono = value
+        End Set
+    End Property
+
+    Public Property Email As String
+        Get
+            Return _email
+        End Get
+        Set(value As String)
+            _email = value
+        End Set
+    End Property
 End Class
